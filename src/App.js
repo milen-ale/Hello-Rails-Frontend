d5824f4220f8e630bb54cc59c4c132e9759d5e7e
milen-ale <milenalebel2@gmail.com>
2023-01-21 10:28:35 +0300

    add app index index file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Heading from './components/Heading';
+import Greeting from './components/Greeting';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="container mx-auto mt-28 px-5 flex flex-col">
+      <Heading />
+      <Routes>
+        <Route path="/" element={<Greeting />} />
+      </Routes>
+    </main>
   );
 }
 
